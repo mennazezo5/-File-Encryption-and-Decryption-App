877c792ae3134ab0d3e1e1ecae2a126f6d6584e6
mennazezo5 <zezomenna6@gmail.com>
2024-08-15 23:33:00 +0300

    two

--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <filesystem>
 using namespace std;
+namespace fs = filesystem;
+const string ENCRYPTED_EXTENSION = ".Hashira";
 
-void caesar_cipher(vector<char> &data, int key, bool encrypt) {
-    for (auto &byte: data) {
-        if (encrypt) {
-            byte += key; // encrypt bytes
-        } else {
-            byte -= key; // decrypt bytes
-        }
+void caesar_cipher(vector<char> &data, int key) {
+    for (char &byte: data) {
+        byte += key;
     }
 }
 
-void process_file(const string &file_path, int key, bool encrypt) {
+void process_file(const string &file_path, int key) {
     // open file to read
     ifstream file(file_path, ios::binary);
     if (!file) {
-        cerr << "Error opening file!" << endl;
+        cerr << "Error opening file!" << std::endl;
         return;
     }
 
@@ -26,42 +25,46 @@ void process_file(const string &file_path, int key, bool encrypt) {
     file.close();
 
     // encrypt or decrypt
-    caesar_cipher(data, key, encrypt);
+    caesar_cipher(data, key);
 
-    // open file to write
-    ofstream out_file(file_path, ios::binary);
-    if (!out_file) {
+    string output_file_path;
+    if (key > 0) {
+        // encrypt
+        output_file_path = file_path + ENCRYPTED_EXTENSION;
+    } else {
+        // decrypt
+        output_file_path = file_path.substr(0, file_path.size() - ENCRYPTED_EXTENSION.size());
+    }
+
+    ofstream output_file(output_file_path, ios::binary);
+    if (!output_file) {
         cerr << "Error opening file for writing!" << endl;
         return;
     }
 
     // write new data
-    out_file.write(data.data(), data.size());
-    out_file.close();
+    output_file.write(data.data(), data.size());
+    output_file.close();
 
-    cout << "Operation completed successfully!" << endl;
+    // delet origin file
+    fs::remove(file_path);
+
+    cout << "Process completed successfully!" << endl;
 }
 
 int main() {
     string file_path;
     int key;
-    string choice;
-
     cout << "Enter the path to the file: ";
     getline(cin, file_path);
 
     cout << "Enter the key for Caesar Cipher (integer value): ";
     cin >> key;
 
-    cout << "Do you want to (1) encrypt or (2) decrypt the file? Enter 1 or 2: ";
-    cin >> choice;
-
-    if (choice == "1") {
-        process_file(file_path, key, true);
-    } else if (choice == "2") {
-        process_file(file_path, key, false);
+    if (fs::path(file_path).extension() == ENCRYPTED_EXTENSION) {
+        process_file(file_path, -key); // decrypt
     } else {
-        cerr << "Invalid choice!" << endl;
+        process_file(file_path, key); // encrypt
     }
 
     return 0;
